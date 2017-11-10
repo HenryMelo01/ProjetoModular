0f1f947a5082578deffc85df80dbed4270047755
Henrique melo <henrique_melo01@outlook.com>
2017-11-10 09:35:15 -0200

    Proposta de sintaxe baseada em MySql com adequacao de campos

--- tables/tabelas_cadastro.sql
+++ tables/tabelas_cadastro.sql
@@ -6,3 +6,14 @@ sexo VARCHAR(1),
 cidade VARCHAR(),
 bairro VARCHAR()
 )
+
+
+CREATE TABLE `bdTesteProjetoModular`.`cadastro` (
+`ID` INT NOT NULL AUTO_INCREMENT 
+, `nome` VARCHAR(50) NOT NULL 
+, `idade` INT NOT NULL 
+, `sexo` BIT NOT NULL 
+, `cidade` VARCHAR(70) NOT NULL 
+, `bairro` VARCHAR(50) NOT NULL 
+, PRIMARY KEY (`ID`)
+) ENGINE = MyISAM COMMENT = 'Esboco da primeira tabela do sistema';
