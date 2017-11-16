345ceacf362254f2faf8c72c9398596506ec024e
Gabriel2303 <gabriel.silva@CAMPUS-00021.local>
2017-11-16 18:05:46 -0200

    fix php_InsereRegistro.php

--- PHP/php_InsereRegistro.php
+++ PHP/php_InsereRegistro.php
@@ -4,14 +4,13 @@
 	$nome = $_POST['inputName'];
 	$idade = $_POST['inputIdade'];
 	$sexo = $_POST['inputSexo'];
-	$cidade = $_POST['inputCidade'];
 	$bairro = $_POST['inputBairro'];
 	if($sexo == "masculino"){
 		$sexo = 1;
 	}else{
 		$sexo = 0;
 	}
-	$query= "INSERT INTO custumer(nome, idade, sexo, cidade, bairro) VALUES ('".$nome."',".$idade.",'".$sexo."','".$cidade."','".$bairro."')";
+	$query= "INSERT INTO custumer(nome, idade, sexo, ID_neighborhood) VALUES ('".$nome."',".$idade.",".$sexo.",".$bairro.")";
 	@mysqli_query($conn, $query);
-	fecharConexao();
+	fecharConexao($conn);
 ?>
